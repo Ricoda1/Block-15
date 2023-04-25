1aef99608349e2b7fd598cf03d35cec4b8782e39
“Ricoda1” <david1997rico@gmail.com>
2023-04-25 09:00:43 -0700

    done with froyo survey

---
+++ index.js
@@ -0,0 +1,31 @@
+// create an object called customer and add the following 
+const customer = {
+    firstName: "jake",
+    lastName: "smith",
+    email: "jaekSmih!aol.com",
+    phone: undefined,
+    zipCode: "631",
+    favoriteFlavors: 32,
+    cupSize: "medium",
+    favoriteStore: "Target",
+    firstVisit: false,
+}
+
+customer["email"] = "Jak3Smith1992@email.com";
+customer["phone"] = 3195551234;
+customer["zipCode"] = "63132";
+customer["favoriteFlavors"] = ["coffee", "strawberry", "matcha"]
+
+delete customer.zipCode
+delete customer.favoriteStore
+
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"]
+customer.futureFlavors = "mango"
+customer.todaysPurchaseCost = 5.32
+
+for(const property in customer ) {
+    console.log(property)
+}
+
+// console.table(customer)
+
